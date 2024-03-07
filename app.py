1ba4cc0b8ed6cfa09bcb3d0cdbca85c009a204ff
jdiph051sj <jdiph051@gmail.com>
2024-03-07 04:13:18 +0200

    Added port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+import os
 
 @app.route("/")
 
@@ -8,4 +9,5 @@ def home():
     return "Hello Equal Experts!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
